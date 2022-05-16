e7413a771b243f3c37511570ac8d863ad4347cb5
AlexandraZbant <alexandrazbant@gmail.com>
2022-05-16 11:18:48 +0300

    Fixing credential visibility

--- src/service/DatabaseConnector.java
+++ src/service/DatabaseConnector.java
@@ -1,5 +1,7 @@
 package service;
 
+import repository.Credentials;
+
 import java.sql.*;
 
 public class DatabaseConnector {
@@ -8,7 +10,7 @@ public class DatabaseConnector {
 
     public static Statement getReadStatement() {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/twitterDatabase", "root", "MySQL3085");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/twitterDatabase", "root", Credentials.PASSWORD);
             return connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
         } catch (SQLException e) {
             System.out.println(CONNECTION_ERROR);
@@ -18,7 +20,7 @@ public class DatabaseConnector {
 
     public static Statement getUpdateStatement() {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/twitterDatabase", "root", "MySQL3085");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/twitterDatabase", "root", Credentials.PASSWORD);
             return connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
         } catch (SQLException e) {
             System.out.println(CONNECTION_ERROR);
